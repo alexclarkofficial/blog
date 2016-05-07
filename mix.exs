3d3e9ff3648a6059aca8fc6faeae5946a1b03fef
Alex Clark <alexclarkofficial@users.noreply.github.com>
2016-05-07 15:02:51 -0400

    Update Elixir and Phoenix to latest version (#2)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixBlog.Mixfile do
   def project do
     [app: :phoenix_blog,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,7 +30,7 @@ defmodule PhoenixBlog.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.3"},
+    [{:phoenix, "~> 1.1"},
      {:phoenix_ecto, "~> 1.1"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
